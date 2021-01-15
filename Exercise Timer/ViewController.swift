ff6fcb219a708e26a8e34f162c8c8e3387c55deb
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2021-01-15 13:24:21 +0000

    Initial Commit

---
+++ Exercise Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise Timer
+//
+//  Created by Tommi Saarikangas on 15.1.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
