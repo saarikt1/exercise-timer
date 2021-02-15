ee0f14ef5cdd27807cccb152e3322eb07a43f438
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2021-02-15 16:39:24 +0200

    Separate view from controller

--- exercise-timer/StartScreenViewController.swift
+++ exercise-timer/StartScreenViewController.swift
@@ -14,50 +14,12 @@ class StartScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLayout()
+        let startScreenView = StartScreenView()
+        view.addSubview(startScreenView)
+        startScreenView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
     }
     
-    private func setupLayout() {
-        view.backgroundColor = .black
-
-        let topImageContainerView = UIView()
-        view.addSubview(topImageContainerView)
-        topImageContainerView.snp.makeConstraints { (make) in
-            make.height.equalToSuperview().multipliedBy(0.4)
-            make.leading.trailing.equalToSuperview()
-        }
-
-        let logo = R.image.logo()
-        let logoImageView = UIImageView()
-        logoImageView.image = logo
-        
-        topImageContainerView.addSubview(logoImageView)
-        logoImageView.snp.makeConstraints { (make) in
-            make.center.equalToSuperview()
-        }
-
-        let bottomMenuContainerView = UIView()
-        view.addSubview(bottomMenuContainerView)
-        bottomMenuContainerView.snp.makeConstraints { (make) in
-            make.height.equalToSuperview().multipliedBy(0.6)
-            make.leading.trailing.equalToSuperview()
-            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
-        }
-
-        let restTimerButton = MenuButton(label: "REST TIMER")
-        let forTimeButton = MenuButton(label: "FOR TIME")
-        let amrapButton = MenuButton(label: "AMRAP")
-        let emomButton = MenuButton(label: "EMOM")
-        let tabataButton = MenuButton(label: "TABATA")
-        
-        let menuStackView = UIStackView(arrangedSubviews: [restTimerButton, forTimeButton, amrapButton, emomButton, tabataButton])
-        
-        bottomMenuContainerView.addSubview(menuStackView)
-        menuStackView.axis = .vertical
-        menuStackView.spacing = 16
-        menuStackView.snp.makeConstraints { (make) in
-            make.center.equalToSuperview()
-        }
-    }
 }
 
